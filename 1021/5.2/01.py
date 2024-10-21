27db79f4c2ad8b4882de6da5b59f992df47720c1
ksk <133188669+ksk-117@users.noreply.github.com>
2024-10-21 11:37:22 +0900

    Fix typo in image file path and uncomment image saving

--- 1021/5.2/01.py
+++ 1021/5.2/01.py
@@ -1,8 +1,8 @@
 import cv2
-img = cv2.imread("pic/Parraots.bmp")
+img = cv2.imread("pic/Parrots.bmp")
 cv2.imshow("img", img)
 dst = cv2.flip(img, 0)
-cv2.imwrite("flip0.jpg, dst")
+#cv2.imwrite("pic/flip0.jpg, dst")
 cv2.imshow("dst 0", dst)
 dst = cv2.flip(img, 1)
 cv2.imshow("dst 1", dst)
