4adfec16c5a4cbec6f603cd7ab4ac2501e296f2e
ksk <133188669+ksk-117@users.noreply.github.com>
2024-10-28 11:05:01 +0900

    Refactor image file paths in Python scripts and update directory name

--- 1028/6.1/01.py
+++ 1028/6.1/01.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread('noise_mono.jpg')
+img = cv2.imread('pic/noise_mono.jpg')
 cv2.imshow('img', img)
 element8 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
 dst1 = cv2.dilate(img, element8, iterations=1)
--- 1028/6.1/02.py
+++ 1028/6.1/02.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-img = cv2.imread('noise_mono.jpg')
+img = cv2.imread('pic/noise_mono.jpg')
 cv2.imshow('img', img)
 element4 = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
-dst1 = cv2.morpfologyEx(img, cv2.MORPH_OPEN, element4, iterations=1)
-dst2 = cv2.morpfologyEx(img, cv2.MORPH_CLOSE, element4, iterations=1)
+dst1 = cv2.morphologyEx(img, cv2.MORPH_OPEN, element4, iterations=1)
+dst2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, element4, iterations=1)
 dst3 = cv2.morphologyEx(dst1, cv2.MORPH_CLOSE, element4, iterations=1)
 cv2.imshow('open', dst1)
 cv2.imshow('close', dst2)
