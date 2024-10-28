323ce1ead1acfe64567f5f51517be4c16682ec4d
ksk <133188669+ksk-117@users.noreply.github.com>
2024-10-28 10:50:42 +0900

    Refactor image file paths in Python scripts and update directory name

--- Creat_File.py
+++ Creat_File.py
@@ -1,14 +1,14 @@
 import os
 
 # ファイルを作成するディレクトリのパスを指定
-directory = "ensyu"  
+directory = "8.2"  
 
 # ディレクトリが存在しない場合は作成
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 # ファイル名の範囲を指定
-for i in range(1, 10):  
+for i in range(1, 6):  # 1 ~ n-1 までのファイルを作成
     filename = os.path.join(directory, f"{i:02}.py")
     with open(filename, 'w') as file:
         pass
