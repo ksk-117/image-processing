36eb818482d93ee0bfc2f3dcd8fbc02dcec686a5
ksk <133188669+ksk-117@users.noreply.github.com>
2024-11-18 13:41:02 +0900

    ディレクトリ名を「11.2」から「10.2」に変更し、作成するファイル数を2から4に増やしました。

--- Creat_File.py
+++ Creat_File.py
@@ -1,14 +1,14 @@
 import os
 
 # ファイルを作成するディレクトリのパスを指定
-directory = "11.2"  
+directory = "10.2"  
 
 # ディレクトリが存在しない場合は作成
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 # ファイル名の範囲を指定
-for i in range(1, 3):  # 1 ~ n-1 までのファイルを作成
+for i in range(1, 5):  # 1 ~ n-1 までのファイルを作成
     filename = os.path.join(directory, f"{i:02}.py")
     with open(filename, 'w') as file:
         pass
